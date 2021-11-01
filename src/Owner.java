027eabe3fa7f52d4a44c94cc38b26972673568e0
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-01 10:23:36 -0400

    wrote comment for future improvments

--- src/Owner.java
+++ src/Owner.java
@@ -3,9 +3,6 @@ import java.util.ArrayList;
 public class Owner {
   String name;
   Integer id = all.size() + 1;
-  // Create an ArrayList containing Pet objects and Pet object subclasses 
-  // If needed to create ArrayList that hold other type that are not 
-  // subclasses (for ex: Owner and Pet )-> ArrayList<Object>
   public ArrayList<Pet> pets = new ArrayList<Pet>();
   static ArrayList<Owner> all = new ArrayList<Owner>();
 
@@ -15,11 +12,7 @@ public class Owner {
   }
 
   static public Owner findById(Integer id) {
-    // for (Owner owner: all ) {
-    //   if (id == owner.id) {
-    //     return owner;   
-    //   }
-    // }
+    // faster if used hashmap
     return all.get(id - 1);
   }
 
